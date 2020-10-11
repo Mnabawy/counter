41ffe98444cd783398ca5ee3f62babfa051ce7e1
MNabawy <m_ninja61@yahoo.com>
2020-10-11 08:29:20 +0200

    default style

--- src/Counter.js
+++ src/Counter.js
@@ -1,45 +1,49 @@
 import React, { useState, useEffect } from 'react';
 
-const getStateFromLocalStorage = () => {
-    const storage = localStorage.getItem('counterState')
-    console.log(storage);
-    if (storage) return JSON.parse(storage).count;
-    return { count: 0 };
-}
+// const getStateFromLocalStorage = () => {
+//     const storage = localStorage.getItem('counterState')
+//     console.log(storage);
+//     if (storage) return JSON.parse(storage).count;
+//     return { count: 0 };
+// }
 
-const storeStateInLocalStorage = count => {
-    localStorage.setItem('counterState', JSON.stringify({ count }))
-    console.log(localStorage);
-}
+// const storeStateInLocalStorage = count => {
+//     localStorage.setItem('counterState', JSON.stringify({ count }))
+//     console.log(localStorage);
+// }
 
-const useLocalStorage = (initialState, key) => {
-    const get = () => {
-        const storage = localStorage.getItem(key)
-        console.log(localStorage, storage);
-        if (storage) return (
-            JSON.parse(storage)[key]
-        )
-        return initialState;
-    }
+// const useLocalStorage = (initialState, key) => {
+//     const get = () => {
+//         const storage = localStorage.getItem(key)
+//         console.log(localStorage, storage);
+//         if (storage) return (
+//             JSON.parse(storage)[key]
+//         )
+//         return initialState;
+//     }
 
-    const [value, setValue] = useState(get()) //decaration of the value variable 
+//     const [value, setValue] = useState(get()) //decaration of the value variable 
 
-    useEffect(() => {
-        localStorage.setItem(key, JSON.stringify({ value }))
-    }, [value])
+//     useEffect(() => {
+//         localStorage.setItem(key, JSON.stringify({ value }))
+//     }, [value])
 
-    return [value, setValue];
-}
+//     return [value, setValue];
+// }
 
 const Counter = ({ max, step }) => {
 
-    const [count, setCount] = useLocalStorage(0, 'count')
+    const [count, setCount] = useState(0);
+    const countRef = React.useRef();
+
+    let message = '';
+    if (countRef.current < count) message = 'Higher';
+    if (countRef.current > count) message = 'Lower';
+
+    countRef.current = count;
 
     const increment = () => {
-        setCount(c => {
-            if (c >= max) return c;
-            return c + step;
-        });
+        setCount(c => c + 1)
     }
 
     const decrement = () => setCount(count - 1);
@@ -50,11 +54,14 @@ const Counter = ({ max, step }) => {
     }, [count])
 
     useEffect(() => {
-        storeStateInLocalStorage(count);
-    })
+        setTimeout(() => {
+            console.log(`Count: ${count}`);
+        }, 3000)
+    }, [count])
 
     return (
         <div className='counter'>
+           <h1> {message}</h1>
             <div className='number'>{count}</div>
             <button onClick={increment}>increment</button>
             <button onClick={decrement}>decrement</button>
